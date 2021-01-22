8839410ee295f53ee848a485a327cd22ef5c5417
CCLancaster <connieclancaster@gmail.com>
2021-01-22 13:48:13 -0800

    tinkering

---
+++ index.js
@@ -0,0 +1,15 @@
+var xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', function() {
+	if (xhr.status !== 200) {
+		return;
+	}
+
+	var data = JSON.parse(xhr.responseText);
+
+	console.log(data);
+});
+
+xhr.open('GET', 'http://www.omdbapi.com/?t=Frozen');
+
+xhr.send();
